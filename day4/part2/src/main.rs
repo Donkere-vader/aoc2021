3d3fdfa56f53b3f000585f579a56ab4ffb55a4bb
donkere-vader <donkere.v@gmail.com>
2021-12-04 08:59:35 +0100

    day 4

---
+++ day4/part2/src/main.rs
@@ -0,0 +1,107 @@
+use std::fs;
+use std::collections::HashMap;
+
+type Board = [[i64; 5]; 5];
+type NumbersMap = HashMap<i64, Vec<(usize, usize, usize)>>;
+
+fn get_puzzle_input() -> (Vec<i64>, Vec<Board>, NumbersMap) {
+    let mut numbers = Vec::new();
+    let mut boards = Vec::new();
+    let mut numbers_mapped: NumbersMap = HashMap::new();
+
+    let file_as_string = fs::read_to_string("../puzzle_input.txt").expect("Failed to read puzzle input");
+    let file_contents: Vec<&str> = file_as_string.split("\n").collect();
+
+    for num in file_contents[0].split(",") {
+        numbers.push(num.parse::<i64>().expect("Failed to parse input"));
+    }
+
+    let n_boards = (file_contents.len() - 1) / 6;
+
+    for board_idx in 0..n_boards {
+        let mut new_board: Board = Default::default();
+
+        for y in 0..5 {
+            let file_row = file_contents[y + (2 + (board_idx * 6))];
+            let mut x = 0;
+            for str_num in file_row.split(" ") {
+                if str_num == "" { continue }
+                let num = str_num.parse::<i64>().unwrap();
+                new_board[y][x] = num;
+
+                numbers_mapped.entry(num).or_insert(Vec::new()).push((board_idx, x, y));
+
+                x += 1;
+            }
+        }
+        boards.push(new_board);
+    }
+
+    (numbers, boards, numbers_mapped)
+}
+
+fn main() {
+    let (numbers, mut boards, numbers_mapped) = get_puzzle_input();
+
+    let mut last_num = 0;
+    let mut winning_board_idx = 0;
+    let mut boards_yet_to_win: Vec<usize> = (0..boards.len()).collect();
+
+    for num in numbers.iter() {
+        // apply number
+        for num_locations in numbers_mapped.get(num) {
+            for num_loc in num_locations {
+                boards[num_loc.0][num_loc.2][num_loc.1] = -1;
+            }
+        }
+
+        // check if won
+        let mut won = false;
+        for (idx, board) in boards.iter().enumerate() {
+            if !boards_yet_to_win.contains(&idx) { continue }
+
+            for row in board {
+                if row.iter().sum::<i64>() == -5 {
+                    won = true;
+                    winning_board_idx = idx;
+                    break;
+                }
+            }
+
+            for x in 0..5 {
+                if board[0][x] + board[1][x] + board[2][x] + board[3][x] + board[4][x] == -5 {
+                    won = true;
+                    winning_board_idx = idx;
+                    break;
+                }
+            }
+
+            if won {
+                last_num = *num;
+                for (idx, value) in boards_yet_to_win.iter().enumerate() {
+                    if *value == winning_board_idx {
+                        boards_yet_to_win.remove(idx);
+                        break;
+                    }
+                }
+                if boards_yet_to_win.len() == 0 {
+                    break;
+                }
+            }
+        }
+        if boards_yet_to_win.len() == 0 {
+            break;
+        }
+    }
+
+    let mut total = 0;
+    for row in boards[winning_board_idx].iter() {
+        for num in row.iter() {
+            if *num > 0 {
+                total += num;
+            }
+        }
+    }
+
+    println!("{} * {} = {}", total, last_num, total * last_num);
+}
